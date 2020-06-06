f7f16713ee3c0c765d93645b82e2702e20db550d
Fagner Nascimento <fagner@novatera.com.br>
2020-06-06 15:12:00 -0300

    Create Instalar-RDWEBCLIENT-Em-HTML5.ps1

---
+++ Instalar-RDWEBCLIENT-Em-HTML5.ps1
@@ -0,0 +1,16 @@
+﻿# INSTALAR RDS WEB CLIENTE EM HTML5
+
+
+New-SelfSignedCertificate -DnsName “SRV.contoso.local” –CertStoreLocation “cert:\LocalMachine\My” -TestRoot # Cria o certificado SSL para o servidor hostname SRV
+
+Install-Module -Name PowershellGet -Force
+
+Install-Module -Name RDWebClientManagement 
+
+Install-RDWebClientPackage
+
+Import-RDWebClientBrokerCert c:\temp\certificados\srv-cert.cer  #Pasta onde você armazenou o certificado do Broker
+
+Publish-RDWebClientPackage -Type Production -Latest
+
+# Acessar Url em https://seu-servidor.com/rdweb/webclient/index.hmtl
